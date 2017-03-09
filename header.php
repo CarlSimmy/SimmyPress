211ad33d9fc7f23d7fda8be00f8caafdb1c9ea32
Simon Jansson <sijansson@gmail.com>
2017-03-09 15:02:56 +0100

    Removing unused styles. Start of new styling.

--- header.php
+++ header.php
@@ -11,8 +11,8 @@
   <meta name="author" content="">
 
   <title>SimmyPress</title>
-  <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
-  <link href="<?php bloginfo( 'template_directory' );?>/blog.css" rel="stylesheet">
+  <link href="<?php bloginfo( 'template_directory' );?>/build/css/main.css" rel="stylesheet">
+
   <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
   <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
@@ -26,9 +26,9 @@
   <!-- Inserting menu -->
   <?php wp_nav_menu( array( 'theme_location' => 'custom-menu' ) ); ?>
 
-  <div class="container">
+  <div class="page-wrapper">
 
-    <div class="blog-header">
-      <h1 class="blog-title"><a href="<?php bloginfo( 'wpurl' );?>"><?php echo get_bloginfo( 'name' ); ?></a></h1>
-      <p class="lead blog-description"><?php echo get_bloginfo( 'description' ); ?></p>
+    <div class="header">
+      <h1 class="header__title"><a href="<?php bloginfo( 'wpurl' );?>"><?php echo get_bloginfo( 'name' ); ?></a></h1>
+      <p class="header__description"><?php echo get_bloginfo( 'description' ); ?></p>
     </div>
